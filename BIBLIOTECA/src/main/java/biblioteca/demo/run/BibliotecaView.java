5d25b723316c17b6ba7378046457f8bbb636313a
jealma929 <jealmaformacion@gmail.com>
2025-03-27 22:20:03 +0100

    27/03/2025 casa

--- BIBLIOTECA/src/main/java/biblioteca/demo/run/BibliotecaView.java
+++ BIBLIOTECA/src/main/java/biblioteca/demo/run/BibliotecaView.java
@@ -97,7 +97,7 @@ public class BibliotecaView {
 	private JPanel prestamo;
 	
 
-	//*********** Devuelve la fecha con incremento
+	//*********** Devuelve la fecha en formato dd/MM/yyyycon incremento
 	public static String asignaFecha (int i) {
 		LocalDate lD = LocalDate.now();
 		lD = lD.plusDays (i);
@@ -107,8 +107,13 @@ public class BibliotecaView {
 	}
 	//*********** Devuelve la fecha a un formato LocalDate 
 	public static String fechaLD (String fecha) {
-		DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter dTF = DateTimeFormatter.ofPattern("ddMMyyyy");
 		DateTimeFormatter dTFDT = DateTimeFormatter.ofPattern("yyyyMMdd");
+		LocalDate lD = LocalDate.parse(fecha,dTF);
+		String slD = lD.format(dTFDT);
+		return slD;
+		
+		
 		
 		
 	}
@@ -176,12 +181,17 @@ public class BibliotecaView {
 		
 		this.datos.add(tFIsbnP.getText());
 		this.datos.add(tFNumSP.getText());
+		
 		String fechaAlta = tFFechaAlta.getText().replace("/", "");
-		int ifechaAlta = Integer.parseInt(fechaAlta);
+		
+		int ifechaAlta = Integer.parseInt(fechaLD(fechaAlta));
 		this.datos.add(ifechaAlta);
+		
 		String fechaDevolucion = tFFechaDevolucion.getText().replace("/","");
-		int ifechaDevolucion=Integer.parseInt(fechaDevolucion);
+		
+		int ifechaDevolucion=Integer.parseInt(fechaLD(fechaDevolucion));
 		this.datos.add(ifechaDevolucion);
+		
 		
 		int numero= Integer.parseInt(datos.get(0).toString());
 		tFAvisosP.setText(fechaAlta+" "+fechaDevolucion+" "+numero);
--- BIBLIOTECA/src/main/resources/data.sql
+++ BIBLIOTECA/src/main/resources/data.sql
@@ -7,14 +7,14 @@ delete from prestamo;
 delete from socio;
 
 INSERT INTO libro(isbn,titulo,autor,edicion,categoria) VALUES 
-(437622149,"El Quijote","Miguel de Cervantes", 2007,1),
+(437622149,"El Quijote","Miguel de Cervantes",2007,1),
 (416434725,"Papá dice", "Margarita del Mazo",2017,0),
 (419507594,"Cuento de Navidad", "Charles Dickens",2003,0),
 (401034794,"El Albatros negro", "Maria Oruña",2025,1);
 
 
 INSERT INTO prestamo(libro,socio,fechaPrestamo,fechaDevolucion) VALUES 
-(437622149,12000001,20250103,"20240218),
+(437622149,12000001,20250103,20240218),
 (416434725,99000001,20250305,20250305);
 
 
